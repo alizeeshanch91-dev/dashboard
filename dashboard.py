126f92ceae00898b08ddc8852c29a2931ddc986c
Ali Zeeshan <alizeeshanch91@gmail.com>
2026-08-14 19:14:50 +0500

    Complete CSV Explorer with charts and summary stats

--- dashboard.py
+++ dashboard.py
@@ -38,15 +38,51 @@ char_type = tk.StringVar(value= "bar")
 dropdown = tk.OptionMenu(root, char_type,"bar","line","scatter")
 dropdown.pack(pady= 10)
 
+stats_label = tk.Label(root, text="")
+stats_label.pack(pady=10)
+
+# later, to update its text:
+stats_label.config(text="new text here")
+
 def plot_chart():
    selection = listbox.curselection()
    if selection:
         selected_col = listbox.get(selection[0])
-        seleted_type = char_type.get()
-        print(f"Column:{selected_col}, chart type:{seleted_type}")
+        selected_type = char_type.get()
+        print(f"Column:{selected_col}, chart type:{selected_type}")
+        try:
+            mean_val = df[selected_col].mean()
+            min_val = df[selected_col].min()
+            max_val = df[selected_col].max()
+            count_val = df[selected_col].count()
+            stats_label.config(text=f"Mean: {mean_val:.2f}  Min: {min_val}  Max: {max_val}  Count: {count_val}")
+        except:
+             stats_label.config(text="Stats not available for this column (non-numeric)")
    else:
        print("no column selected!")
+   ax.clear()
+   if selected_type == "bar":
+       ax.bar(df.index,df[selected_col])
+   elif selected_type == "line":
+       ax.plot(df.index,df[selected_col])
+   elif selected_type == "scatter":
+       ax.scatter(df.index,df[selected_col])
+
+   canvas.draw()
+
 plot_button = tk.Button(root,text="plot",command = plot_chart)
 plot_button.pack(pady = 10)
+
+from matplotlib.figure import Figure
+fig = Figure(figsize=(5, 4))
+ax = fig.add_subplot(111) 
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().pack(pady=10)
+
+ax.bar(["a","b","c"], [1,2,3])
+canvas.draw()
+ax.clear()
 
 root.mainloop() # keeps the window open until the close button is clicked
